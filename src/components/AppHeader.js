9d93670220379da4dfe4220473ebce8b4eee3034
kendallvision <greg.kendall@comcast.net>
2018-04-08 07:45:30 -0400

    refactor for header component

---
+++ src/components/AppHeader.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import '../styles/AppHeader.css';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div className="appHeader">
+                <div>
+                    <h1 className="appHeader-title">Warriors for Christ</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AppHeader;
