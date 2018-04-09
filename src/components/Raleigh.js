65610058d2aa4678159d07ef14f858fcabec49bf
kendallvision <greg.kendall@comcast.net>
2018-04-09 19:22:49 -0400

    fixing text sizes

--- src/components/Raleigh.js
+++ src/components/Raleigh.js
@@ -7,7 +7,7 @@ class Raleigh extends Component {
         return (
             <div className="location">
                 <h3>Raleigh</h3>
-                <h4>Brooks Avenue Church of Christ</h4>
+                <h4>Brooks Ave Church of Christ</h4>
                 <p>700 Brooks Ave<br />Raleigh, NC 27607</p>
                 <p>Tuesdays at 6pm</p>
             </div>
