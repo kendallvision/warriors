ffcb66ab25e23acf005f1a57dba3ad411e912b3c
kendallvision <greg.kendall@comcast.net>
2018-04-10 08:10:20 -0400

    refactor to componentize location objects

--- src/components/Atlanta.js
+++ src/components/Atlanta.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react'
+import Location from './Location'
 
-import '../styles/Locations.css'
+class Atlanta extends Location { }
 
-class Atlanta extends Component {
-    render() {
-        return (
-            <div className="location">
-                <h3>Atlanta Location</h3>
-                <div className="headInstructor"><b>Head Instructor:</b> Greg Kendall, sensei</div>
-                    <div className="locationAddress">
-                        <a href="https://www.google.com/maps/place/Gwinnett+Church+of+Christ/@33.9811122,-84.0559891,15z/data=!4m2!3m1!1s0x0:0x1868baf4c86a77b0?sa=X&ved=0ahUKEwj905HMza_aAhUp_4MKHfFJBLUQ_BIIkwEwCg" target="blank">
-                            <h4>Gwinnett Church of Christ</h4>
-                            <h5>1736 Sever Rd<br/>Lawrenceville, GA 30043</h5>
-                        </a>
-                    </div>
-                <h4>Classes held Thursdays at 6pm</h4>
-                <div className="locationDues">
-                    <b>Dues</b>
-                    <ul>
-                        <li>Currently accepting new students</li>
-                        <li>Registration $50 (includes 1st month &amp; uniform)</li>
-                        <li>Monthly dues $25 for first student</li>
-                        <li>$5 for each additional family member</li>
-                    </ul>
-                </div>
-                <p>
-                    <a href="mailto:info.atlanta@warriors-for-christ.com">Email Atlanta Dojo</a>
-                </p>
-            </div>
-        );
-    }
+Atlanta.defaultProps = {
+    locationTown: "Atlanta",
+    headInstructor: "Greg Kendall, sensei",
+    mapLink: "https://www.google.com/maps/place/Gwinnett+Church+of+Christ/@33.9811122,-84.0559891,15z/data=!4m2!3m1!1s0x0:0x1868baf4c86a77b0?sa=X&ved=0ahUKEwj905HMza_aAhUp_4MKHfFJBLUQ_BIIkwEwCg",
+    locationName: "Gwinnett Church of Christ",
+    address1: "1736 Sever Rd",
+    cityStateZip: "Lawrenceville, GA 30043",
+    classTimes:"Thursdays at 6pm",
+    registrationStatus: "Currently accepting new students",
+    registrationPrice: "Registration $50 (includes 1st month & uniform)",
+    registrationMonthly: "Monthly dues $25 for first student",
+    registrationOtherFamily: "$5 for each additional family member",
+    infoEmail: "mailto:info.atlanta@warriors-for-christ.com"
+
 }
 
 export default Atlanta;
--- src/components/Raleigh.js
+++ src/components/Raleigh.js
@@ -1,35 +1,21 @@
 import React, { Component } from 'react'
+import Location from './Location'
 
-import '../styles/Locations.css'
+class Raleigh extends Location { }
 
-class Raleigh extends Component {
-    render() {
-        return (
-            <div className="location">
-                <h3>Raleigh Location</h3>
-                <div className="headInstructor"><b>Head Instructor:</b> Joel Geniesse, soke</div>
-                <div className="locationAddress">
-                    <a href="https://www.google.com/maps/place/Brooks+Avenue+Church+of+Christ/@35.7971213,-78.6721486,15z/data=!4m2!3m1!1s0x0:0xd89c8965f9fe4610?sa=X&ved=0ahUKEwjVoJ7wzq_aAhXDy4MKHW5KAdkQ_BIIgAEwDQ" target="blank">
-                        <h4>Brooks Ave Church of Christ</h4>
-                        <h5>700 Brooks Ave<br />Raleigh, NC 27607</h5>
-                    </a>
-                </div>
-                <h4>Classes held Tuesdays at 6pm</h4>
-                <div className="locationDues">
-                    <b>Dues</b>
-                    <ul>
-                        <li>Email us to be added to our new student waiting list.</li>
-                        <li>Registration $50 (includes 1st month &amp; uniform)</li>
-                        <li>Monthly dues $30 for first student</li>
-                        <li>$10 for each additional family member</li>
-                    </ul>
-                </div>
-                <p>
-                    <a href="mailto:info.raleigh@warriors-for-christ.com">Email Raleigh Dojo</a>
-                </p>
-            </div>
-        );
-    }
+Raleigh.defaultProps = {
+    locationTown: "Raleigh",
+    headInstructor: "Joel Geniesse, soke",
+    mapLink: "https://www.google.com/maps/place/Brooks+Avenue+Church+of+Christ/@35.7971213,-78.6721486,15z/data=!4m2!3m1!1s0x0:0xd89c8965f9fe4610?sa=X&ved=0ahUKEwjVoJ7wzq_aAhXDy4MKHW5KAdkQ_BIIgAEwDQ",
+    locationName: "Brooks Church of Christ",
+    address1: "700 Brooks Ave",
+    cityStateZip: "Raleigh, NC 27607",
+    classTimes:"Tuesdays at 6pm",
+    registrationStatus: "Email us to be added to our new student waiting list.",
+    registrationPrice: "Registration $50 (includes 1st month & uniform)",
+    registrationMonthly: "Monthly dues $30 for first student",
+    registrationOtherFamily: "$10 for each additional family member",
+    infoEmail: "mailto:info.raleigh@warriors-for-christ.com"
 }
 
 export default Raleigh;
