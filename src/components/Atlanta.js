08b79b0f8b9b744bc917f432661a61d9f791edbe
kendallvision <greg.kendall@comcast.net>
2018-04-09 21:04:11 -0400

    adding pricing and fixing spacing

--- src/components/Atlanta.js
+++ src/components/Atlanta.js
@@ -9,7 +9,14 @@ class Atlanta extends Component {
                 <h3>Atlanta</h3>
                 <h4>Gwinnett Church of Christ</h4>
                 <p>1736 Sever Rd<br/>Lawrenceville, GA 30043</p>
-                <p>Thursdays at 6pm</p>
+                <p>Classes held Thursdays at 6pm</p>
+                <p>Dues
+                    <ul>
+                        <li>Registration $50 (includes 1st month &amp; uniform)</li>
+                        <li>Monthly dues $25 for first student</li>
+                        <li>$5 for each additional family member.</li>
+                    </ul>
+                </p>
             </div>
         );
     }
