f2a328c48d5ed2632694a0370463ab13168f7094
kendallvision <greg.kendall@comcast.net>
2018-04-11 19:58:53 -0400

    fixing typo

--- src/components/Raleigh.js
+++ src/components/Raleigh.js
@@ -4,7 +4,7 @@ class Raleigh extends Location { }
 
 Raleigh.defaultProps = {
     locationTown: "Raleigh",
-    headInstructor: "Joel Geniesse, soke",
+    headInstructor: "Joel Geniesse, Soke",
     mapLink: "https://www.google.com/maps/place/Brooks+Avenue+Church+of+Christ/@35.7971213,-78.6721486,15z/data=!4m2!3m1!1s0x0:0xd89c8965f9fe4610?sa=X&ved=0ahUKEwjVoJ7wzq_aAhXDy4MKHW5KAdkQ_BIIgAEwDQ",
     locationName: "Brooks Ave Church of Christ",
     address1: "700 Brooks Ave",
