5450b9499733af551ae1ff035caface94e542ca2
kendallvision <greg.kendall@comcast.net>
2018-04-11 21:03:34 -0400

    remove link from main logo

--- src/components/WarriorsLogo.js
+++ src/components/WarriorsLogo.js
@@ -8,16 +8,10 @@ import logo from '../images/logoWarriors.jpg'
 import '../App.css';
 
 class WarriorsLogo extends Component {
-
-    appLogoClick(event) {
-        window.scroll(0, 0);
-        ReactDOM.render(<AboutTHWS />, document.getElementById('contentRoot'));
-    }
-
     render() {
         return (
             <div className="mainLogo">
-                <div className="appLogo" onClick={(e) => this.appLogoClick()}>
+                <div className="appLogo">
                     <div className="appLogo-semantic">
                         <img src={logo} alt="logo" /> 
                     </div>
