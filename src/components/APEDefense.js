9759bfb93f95b060e13175ba8c35758846673955
kendallvision <greg.kendall@comcast.net>
2018-04-12 08:07:40 -0400

    add contact email to APE section

--- src/components/APEDefense.js
+++ src/components/APEDefense.js
@@ -18,14 +18,24 @@ class APEDefense extends Component {
                     </p>
                     <p>
                         Attendees are taught defense-minded principles and practical techniques they can use without studying a 
-                        full course on martial arts.  They will use easy, fine motor skills they use each day such as turning a 
+                        full course on martial arts.  They will use easy fine motor skills they use each day such as turning a 
                         key in the car ignition.
                     </p>
+                    <p>
+                        These courses are generally 1-2 hours and offered free of charge.
+                    </p>
                 </div>
-                <UnderConstruction/>
+
+                <div id="contact">
+                    <a href={this.props.infoEmail}>Contact Us To Schedule One</a>
+                </div>
             </div>
         )
     }
+}
+
+APEDefense.defaultProps = {
+    infoEmail: 'info@warriors-for-christ.com'
 }
 
 export default APEDefense;
