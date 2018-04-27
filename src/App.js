052f560a3f4e72693e7db3464f51c91b48b1dc03
kendallvision <greg.kendall@comcast.net>
2018-04-27 10:35:23 -0400

    Fixes #80 - Adding initial redirect for THWS domain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, HashRouter } from "react-router-dom";
+import { Route, HashRouter, Redirect } from "react-router-dom";
   
 import AboutTHWS from './components/AboutTHWS';
 import AppHeader from './components/AppHeader'
@@ -9,8 +9,21 @@ import MainPage from './components/MainPage'
 
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  componentDidMount() {
+    console.log('Location: ' + window.location);
+  }
+
   render() {
+
+    var redirect = '';
+
+    if ( window.location.hostname === 'taohsiehwushu.com' 
+      && (window.location.href.includes('about') === false ) ) {
+        redirect = (<Redirect to='/about'/>);
+    }
+
     return (
       <HashRouter>
           <div>
@@ -21,10 +34,9 @@ class App extends Component {
               <Route path="/geniessej" component={JoelGeniesse}/>
               <Route path="/about" component={AboutTHWS}/>
             </div>        
+            {redirect}
           </div>
       </HashRouter>
     );
   }
 }
-
-export default App;
