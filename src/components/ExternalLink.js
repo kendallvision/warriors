128e246c66bfe98962288dbfb4cc0d22016ec759
Greg Kendall <sensei.kendall@gmail.com>
2019-07-05 09:53:07 -0400

    initial conversion and cleanup

--- src/components/ExternalLink.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ExternalLink extends Component {
-    render() {
-        return (
-            <a target="none" href={this.props.href}>{this.props.desc}</a>
-        )
-    }
-};
